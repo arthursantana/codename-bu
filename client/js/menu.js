c6444b05cac7f418a19e4af6f7c412dd2f6ee9e6
Arthur Santana <rockslave@gmail.com>
2014-10-12 19:20:55 -0300

    primeira tentativa de Phaser + getUserMedia

--- client/js/menu.js
+++ client/js/menu.js
@@ -1,4 +1,12 @@
 function MenuState () {
+   var background;
+   var ground;
+   var dude;
+   var dude2;
+   var title;
+   var startButton;
+   var text;
+
    this.preload = function () {
       game.load.image('title', 'img/title.png');
       game.load.image('button', 'img/button.png');
@@ -9,18 +17,18 @@ function MenuState () {
 
 
    this.create = function () {
-      var background = game.add.sprite(0, 0, 'background');
-      var ground = game.add.tileSprite(0, 400, 335, 112, 'ground');
+      background = game.add.sprite(0, 0, 'background');
+      ground = game.add.tileSprite(0, 400, 335, 112, 'ground');
       
       ground.autoScroll(-300, 0);
 
-      var title = game.add.sprite(144,150,'title');
+      title = game.add.sprite(144,150,'title');
       title.anchor.setTo(0.5,0.5);
 
       game.add.tween(title).to({y:115}, 350, Phaser.Easing.Sinusoidal.InOut, true, 0, 1000, true);
 
-      var dude = game.add.sprite(100,354,'dude');  
-      var dude2 = game.add.sprite(150,354,'dude');  
+      dude = game.add.sprite(100,352,'dude');  
+      dude2 = game.add.sprite(150,354,'dude');  
 
       dude.animations.add('walk', [5,6,7,8], 15, true);
       dude.animations.play('walk');
@@ -28,12 +36,33 @@ function MenuState () {
       dude2.animations.play('walk');
 
       game.physics.startSystem(Phaser.Physics.ARCADE);
-      game.physics.arcade.gravity.y = 500;
+      game.physics.arcade.gravity.y = 1200;
 
       game.physics.arcade.enable(dude2);
       game.physics.arcade.enable(ground);
 
-      var startButton = game.add.button(game.width/2, 300, 'button', function () { game.state.start('game'); }, this);
+      ground.body.allowGravity = false;
+      ground.body.immovable = true;
+
+      text = game.add.text(game.world.centerX, game.world.centerY, "Please allow microphone access \\o/", {
+         font: "12 Arial",
+         fill: "#ffffff",
+         align: "center"
+      });
+      text.anchor.setTo(0.5, 0.5);
+
+      game.input.onDown.add(function () {
+         this.body.velocity.y = -800;
+      }, dude2);
+
+      startButton = game.add.button(game.width/2, 300, 'button', function () { game.state.start('game'); }, this);
       startButton.anchor.setTo(0.5,0.5);
+   };
+
+   this.update = function () {
+      game.physics.arcade.collide(dude2, ground);
+      text.setText("Volume: " + volume);
+      if (dude2.body.touching.down && volume > 0.4)
+         dude2.body.velocity.y = -800;
    };
 }
